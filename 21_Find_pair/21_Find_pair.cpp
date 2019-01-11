7e32dfb6e1625ace1cd0f4b61c97fcca73d7f3a3
Gaspare Ferraro <ferraro@gaspa.re>
2019-01-11 16:03:26 +0100

    initial

---
+++ 21_Find_pair/21_Find_pair.cpp
@@ -0,0 +1,39 @@
+/*
+  Author: Gaspare Ferraro <ferraro@gaspa.re>
+  Problem: <url>
+
+  Solution description:
+
+
+  Time  complexity: O()
+  Space complexity: O()
+
+  Where:
+
+*/
+
+#include <iostream>
+#include <bitset>
+#include <functional>
+#include <utility>
+#include <tuple>
+#include <limits>
+#include <string>
+#include <array>
+#include <vector>
+#include <deque>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+
+  /* Solution */
+
+  return 0;
+}
