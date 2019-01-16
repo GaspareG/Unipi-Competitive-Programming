46dc93c5881ab18d914bfe5cb588a2d9c8989719
Gaspare Ferraro <ferraro@gaspa.re>
2019-01-16 23:04:32 +0100

    24

--- 24_Nested_segments/24_Nested_segments.cpp
+++ 24_Nested_segments/24_Nested_segments.cpp
@@ -1,15 +1,18 @@
 /*
   Author: Gaspare Ferraro <ferraro@gaspa.re>
-  Problem: <url>
+  Problem: <https://codeforces.com/problemset/problem/652/D?locale=en>
 
   Solution description:
 
+    Use a fenwick tree to count how many segments are in range,
+    to do this compress all the coordinates, sort by end positions and
+    query the ft.
 
-  Time  complexity: O()
-  Space complexity: O()
+  Time  complexity: O(N*log(N))
+  Space complexity: O(N)
 
   Where:
-
+  - N is the number of segments
 */
 
 #include <iostream>
@@ -27,6 +30,7 @@
 #include <stack>
 #include <queue>
 #include <algorithm>
+#include <numeric>
 #include <cmath>
 
 template <typename T>
@@ -37,32 +41,52 @@ class fenwick
 
   public:
 
-    fenwick(int size) : N(size)
-    {
-      ft = std::vector<T>(N+1, 0);
-    }
+    fenwick(int size) : N(size){ ft = std::vector<T>(N+1, 0); }
 
-    T query(int b)
+    void update(int k, T v){ for (; k <= N; k += (k&(-k))) ft[k] += v; }
+
+    T query(int b, T sum = 0)
     {
-      T sum = 0;
       for (; b; b -= (b&(-b))) sum += ft[b];
       return sum;
     }
 
-    void update(int k, T v)
-    {
-      for (; k <= N; k += (k&(-k))) ft[k] += v;
-    }
 };
 
 int main()
 {
   std::ios_base::sync_with_stdio(false);
 
-  int N;
-  std::vector<std::array<int, 2>> S;
-  std::vector<int> V(N);
-  std::iota(V.begin(), V.end(), 0);
-  for(int i=0; i<V; i++) std::cin >> S[i][0] >> S[i][1];
+  // Read input
+  int N, count = 0;
+  std::cin >> N;
+  std::vector<int> S(N), E(N);
+  std::vector<int> idx(N), sol(N);
+  std::map<int, int> X;
+
+  for(int i=0; i<N; i++) std::cin >> S[i] >> E[i];
+
+  // Compress coordinates
+  for(int i=0; i<N; i++) X[S[i]] = X[E[i]] = 0;
+  for(auto [k, v] : X) X[k] = ++count;
+  for(int i=0; i<N; i++) S[i] = X[S[i]], E[i] = X[E[i]];
+
+  // Sort segments by end position
+  std::iota(idx.begin(), idx.end(), 0);
+  std::sort(idx.begin(), idx.end(), [&](int a, int b){ return E[a] < E[b]; });
+
+  // Compute solution for each segment
+  fenwick<int> ft(2*N);
+  for(int i=0; i<N; i++)
+  {
+    int id = idx[i];
+    // Number of segments in [L, R]
+    sol[id] = ft.query(E[id]) - ft.query(S[id]-1);
+    ft.update(S[id], 1); // Add segment to ft
+  }
+
+  // Print solutions
+  for(int i=0; i<N; i++) std::cout << sol[i] << std::endl;
+
   return 0;
 }
--- 28_Array_copy/28_Array_copy.cpp
+++ 28_Array_copy/28_Array_copy.cpp
@@ -29,11 +29,128 @@
 #include <algorithm>
 #include <cmath>
 
+struct SegmentTree
+{
+
+  int N;
+  std::vector<std::array<long long int, 2>> LU;
+  std::vector<long long int> ST;
+
+  SegmentTree(int n)
+  {
+    N = n;
+    ST.resize(N, 0);
+    LU.resize(N*4, {-1, -1});
+    for(int i=0; i<N; i++) ST[i] = -i;
+  }
+
+  void update(int x, int y, int k){ update(1, 0, N-1, x, x+k-1, y, y+k-1); }
+
+  // [bs, be] = [as, ae]
+  void update(int idx, int s, int e, int as, int ae, int bs, int be)
+  {
+    if(e < be)
+    {
+      int diffe = be-e;
+      be -= diffe;
+      ae -= diffe;
+    }
+
+    if(s > bs)
+    {
+      int diffs = s-bs;
+      bs -= diffs;
+      as -= diffs;
+    }
+
+    std::cout << idx << "["<< s << ", " << e << "] [" << as << ", " << ae << "] [" << bs <<", " << be << "]" << std::endl;
+
+    if(s > e || s > be || e < bs){ std::cout << "\tCLOSE" << std::endl; return; }
+
+
+    if(s == e)
+    {
+      ST[s] = as;
+      return;
+    }
+
+    if(s >= bs && e <= be)
+    {
+      LU[idx] = {as, ae};
+      return;
+    }
+
+    int  m = ( s+ e)/2;
+
+    update(2*idx  , s  , m, as, ae, bs, be);
+    update(2*idx+1, m+1, e, as, ae, bs, be);
+  }
+
+  long long int query(int x)
+  {
+    return query(1, 0, N-1, x);
+  }
+
+  long long int query(int idx, int s, int e, int x)
+  {
+    if(s > x || e < x) return N+1;
+
+    if(!(LU[idx][0] == -1 && LU[idx][1] == -1))
+    {
+      if(s == e)
+      {
+        ST[idx] = LU[idx][0];
+      }
+      else if(s+1 == e)
+      {
+        ST[2*idx  ] = LU[idx][0];
+        ST[2*idx+1] = LU[idx][1];
+      }
+      else
+      {
+        int mlu = (LU[idx][0]+LU[idx][1])/2;
+        LU[2*idx  ] = {LU[idx][0], mlu};
+        LU[2*idx+1] = {mlu+1, LU[idx][1]};
+      }
+      LU[idx] = {-1, -1};
+    }
+
+    if(s == e) return ST[s];
+
+    int m = (s+e)/2;
+    int l = query(2*idx  , s  , m, x);
+    int r = query(2*idx+1, m+1, e, x);
+
+    return (l == N+1) ? r : l;
+  }
+};
+
 int main()
 {
   std::ios_base::sync_with_stdio(false);
 
-  /* Solution */
+  int N, M;
+  int t, x, y, k;
+  std::cin >> N >> M;
+  std::vector<int> A(N+1), B(N+1);
+  struct SegmentTree st(N+1);
 
+  for(int i=1; i<=N; i++) std::cin >> A[i];
+  for(int i=1; i<=N; i++) std::cin >> B[i];
+  for(int i=0; i<M; i++)
+  {
+    std::cin >> t;
+    if(t == 1)
+    {
+      std::cin >> x >> y >> k;
+      st.update(++x, ++y, k);
+    }
+    else
+    {
+      std::cin >> x;
+      int idx = st.query(++x);
+      std::cout << (idx < 0 ? B[-idx] : A[idx]) << std::endl;
+    }
+  }
   return 0;
 }
